00dbae38d3f92e50b85efa5bb40541b9a5758a81
Juan Carlos Donado Vanegas <54679777+juancDonado@users.noreply.github.com>
2022-05-25 11:30:28 -0500

    Nuevos ejercicios

--- Datos-y-operadores/JS/matrices.js
+++ Datos-y-operadores/JS/matrices.js
@@ -24,13 +24,13 @@ let paises = ['Argentina', 'Australia', 'Belgica', 'Brasil', 'Chile', 'Egipto', 
 let size = paises.length;
 console.log(`El tamaño del array de paises es: ${size}`);
 
-for(let i = 0; i < paises.length; i++){
+/* for(let i = 0; i < paises.length; i++){
     if(paises[i]){
         console.log(`El pais en la posicion ${i+1} es: ${paises[i]}`);
     }else{
         console.log('No existe');
     }
-}
+} */
 
 console.log(`El pais en la posicion 3: ${paises[3]}`);
 console.log(`El pais en la posicion 0: ${paises[0]}`);
@@ -39,3 +39,75 @@ console.log(`El pais en la posicion 4: ${paises[4]}`);
 console.log(`El pais en la posicion 15: ${paises[15]}`);
 console.log(`El pais en la posicion 8: ${paises[8]}`);
 console.log(`El pais en la posicion 13: ${paises[13]}`);
+
+
+
+let pokemons = new Map();
+
+pokemons.set('nombre', 'pikachu'),
+pokemons.set('atack', 90);
+
+for(let [key, value] of pokemons){
+    console.log(key+" - "+value);
+}
+
+let mySet = new Set();
+
+mySet.add(1);
+mySet.add(true);
+mySet.add(()=> 1);
+mySet.add('Hello world');
+
+for(let key of mySet){
+    console.log(key)
+}
+
+
+//Ejercicio #2
+
+let countriesMap = new Map();
+let countriesSet = new Set();
+let countriesArray = ['Panama', 'Egipto', 'Argentina'];
+
+//inicializacion con Map
+countriesMap.set('Pais1', 'Panama');
+countriesMap.set('Pais2', 'Egipto');
+countriesMap.set('Pais3', 'Argentina');
+console.log(countriesMap);
+
+//inicializacion con Set
+countriesSet.add('Panama');
+countriesSet.add('Egipto');
+countriesSet.add('Argentina');
+console.log(countriesSet);
+
+//Ejercicio Map
+countriesMap.set('Pais4', 'Francia');
+console.log(countriesMap);
+countriesMap.delete('Pais2');
+console.log(countriesMap);
+countriesMap.set('Pais5', 'Australia');
+console.log(countriesMap);
+countriesMap.set('Pais6', 'Brasil');
+console.log(countriesMap);
+
+//Ejercicio Set
+countriesSet.add('Francia');
+console.log(countriesSet);
+countriesSet.delete('Egipto');
+console.log(countriesSet);
+countriesSet.add('Brasil');
+console.log(countriesSet);
+
+//Ejercicio Array
+console.log(countriesArray);
+countriesArray.push('Francia');
+console.log(countriesArray);
+countriesArray.splice(1,1);
+console.log(countriesArray);
+countriesArray.push('Australia');
+console.log(countriesArray);
+countriesArray.push('Brasil');
+console.log(countriesArray);
+countriesArray.sort();
+console.log(countriesArray);
