59a2a6029018c8c6ed7c45481167639c8269e6fc
Juan Carlos Donado Vanegas <54679777+juancDonado@users.noreply.github.com>
2022-05-26 11:18:01 -0500

    Ciclos

--- Ciclos/JS/app.js
+++ Ciclos/JS/app.js
@@ -1,1 +1,27 @@
-let array = [];
+let resultado = 0;
+
+for(let i = 10; i >= 0; i--){
+    resultado += i;
+}
+console.log(resultado);
+
+const calificaciones = [90.5, 87.4, 65.6, 40.57];
+
+let suma = 0;
+
+for(let i = 0; i < calificaciones.length; i++){
+    suma += calificaciones[i];
+}
+
+let promedio = suma / calificaciones.length;
+
+console.log(promedio);
+
+
+//ejercicios de foreach
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8];
+
+numeros.forEach((numero, i, z) =>{
+    console.log(`Actuamente estas en la posicion ${i+1} con el valor de ${numero} - ${z}`);
+});
---
+++ Ciclos/JS/calificaciones.js
@@ -0,0 +1,47 @@
+let calificaciones = [];
+
+alert('Digita calificaciones una por una, 0 para finalizar');
+
+let nota;
+
+do{
+    nota = prompt('Ingresa una nota')
+    if(nota > 0){
+        calificaciones.push(nota);
+    }
+}while(nota != 0);
+
+let calificaionMasAlta = 0;
+
+calificaciones.forEach((calificacion)=>{
+    if(calificacion > calificaionMasAlta){
+        calificaionMasAlta = calificacion;
+    }
+});
+
+alert(`Las calificaciones son: ${calificaciones}
+La nota mas alta es: ${calificaionMasAlta}`)
+console.log(calificaciones);
+console.log(calificaionMasAlta);
+/* 
+let coleccionM = new Map();
+let notaM;
+
+do{
+    let i = 0;
+    notaM = prompt('Digita la nota');
+    if(notaM > 0){
+        coleccionM.set(`${i++}`, nota);
+    }
+}while(notaM != 0);
+
+let notaAlta = 0;
+
+coleccionM.forEach((data)=>{
+    if(data > notaAlta){
+        notaAlta = data;
+    }
+});
+
+console.log(coleccionM)
+console.log(notaAlta) */
