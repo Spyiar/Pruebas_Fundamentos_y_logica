943990b46263dc4e0ca0c550c0dc2e2345a62e77
Juan Carlos Donado Vanegas <54679777+juancDonado@users.noreply.github.com>
2022-05-27 09:42:06 -0500

    login arreglos

--- Introduccion/JS/app.js
+++ Introduccion/JS/app.js
@@ -1,24 +1,30 @@
 console.log('Funciona');
+/* let usuario = {
+    nombre : 'juan',
+    password : '12345'
+}
+localStorage.setItem('usuario', JSON.stringify(usuario)); */
 
-let registro = JSON.parse(localStorage.getItem('usuario'));
-let intentos = 0;
-let intentosResetPass = 1;
-let forgetPass = true;
+//let intentos = 0;
+
 
 function restablecerPass(){
+    let intentosResetPass = 1;
     while(intentosResetPass <= 5){
         let passNew = prompt('Nueva contraseña');
         let confirmPass = prompt('Confirmar contraseña');
     
-        if((passNew == '' || passNew == null || !isNaN(passNew)) || (confirmPass == '' || confirmPass == null)){
-            alert('Campos invalidos');
+        if((passNew == '' || passNew == null) || (confirmPass == '' || confirmPass == null)){
+            alert(`Campos invalidos, tienes ${intentosResetPass} de 5 intentos`);
             restablecerPass();
         }
         if (passNew != confirmPass){
             intentosResetPass++;
-            alert(`Las contraseñas no coinciden, tienes ${intentosResetPass} de 5`);
+            alert(`Las contraseñas no coinciden, tienes ${intentosResetPass} de 5 intentos`);
+            restablecerPass();
         }else{
             alert('contraseña creada correctamente');
+            intentosResetPass = 5;
             return passNew;
         }
 
@@ -26,12 +32,17 @@ function restablecerPass(){
             alert('Tu cuenta sera bloqueada');
             return;
         } 
+
+        return passNew;
     }
 }
 
 function validar(){
+    let registro = JSON.parse(localStorage.getItem('usuario'));
+    
     let user = prompt('Digita tu nombre');
     let pass = prompt('Digita tu contraseña');
+
     if((user == '' || user == null || !isNaN(user)) || (pass == '' || pass == null)){
         alert('Usuario o contraseña invalidos');
         validar();
@@ -42,9 +53,19 @@ function validar(){
     }
 
     if(usuario.nombre != registro.nombre || usuario.password != registro.password){
-        let newPass = restablecerPass();
-        usuario.password = newPass;
-        localStorage.setItem('usuario', usuario);
+        alert('Usuario o contraseña incorrectos');
+        let changePass = confirm('¿Deseas cambiar tu contraseña?');
+        if(changePass){
+            let newPass = restablecerPass();
+            usuario.password = newPass;
+            console.log(JSON.stringify(usuario));
+            localStorage.setItem('usuario', JSON.stringify(usuario));
+            validar();
+        }else{
+            validar();
+        }
+    }else{
+        return alert('Iniciado correctamente');
     }
 }
 
